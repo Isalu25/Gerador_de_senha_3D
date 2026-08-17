9006648a60f5680d3db387a0de83f0a44c7508b7
Isalu25 <isabella.luzzi@escola.pr.gov.br>
2026-08-17 08:42:21 -0300

    avalição

---
+++ main.js
@@ -0,0 +1,67 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.parametro-senha__checkbox');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) { alfabeto += letrasMaiusculas; }
+    if (checkbox[1].checked) { alfabeto += letrasMinusculas; }
+    if (checkbox[2].checked) { alfabeto += numeros; }
+    if (checkbox[3].checked) { alfabeto += simbolos; }
+
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.floor(Math.random() * alfabeto.length);
+        senha += alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
+}
+
+function classificaSenha(tamanhoAlfabeto) {
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+
+    if (entropia > 57) {
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35) {
+        forcaSenha.classList.add('media');
+    } else {
+        forcaSenha.classList.add('fraca');
+    }
+}
+geraSenha();
+const emojisCarinhas = ['😀','😎','🤖','👾'];
+const emojisAnimais = ['🦁','🦊','🐼','🦅'];
+if (checkAnimais.checked) {
+    senha += arrayAleatorio(emojisAnimais);
+}
